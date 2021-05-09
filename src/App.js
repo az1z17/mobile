ad17768c7b3910d419b30d6a49b92d0d8f047bb5
Aziz Gulmurodov <3329812.aa@gmail.com>
2021-05-09 22:04:50 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='middle'>
+        <h1>Данные паспорта</h1>
+        <div className='body'>
+          <img className='image' src='ceo.png' alt='person' />
+        </div>
+        <div className='card'>
+          <div className='childCard'>
+            <h3>Личные данные</h3>
+            <div className='row'>
+              <h4>Имя</h4>
+              <h6>Jeck</h6>
+            </div>
+            {/*  */}
+            <div className='row'>
+              <h4>Фамилия</h4>
+              <h6>Jeck</h6>
+            </div>
+            {/*  */}
+            <div className='row'>
+              <h4>Отчество</h4>
+              <h6>Jeck</h6>
+            </div>
+            {/*  */}
+            <div className='row'>
+              <h4>Место рождения</h4>
+              <h6>Jeck</h6>
+            </div>
+            {/*  */}
+            <div className='row'>
+              <h4>Дата рождения</h4>
+              <h6>Jeck</h6>
+            </div>
+            {/*  */}
+            <div className='row'>
+              <h4>Пол</h4>
+              <h6>Jeck</h6>
+            </div>
+            {/*  */}
+          </div>
+        </div>
+        {/*  */}
+        {/*  */}
+        <div className='card'>
+          <div className='childCard'>
+            <h3>Паспортные данные</h3>
+            <div className='row'>
+              <h4>ПИНФЛ</h4>
+              <h6>Jeck</h6>
+            </div>
+            {/*  */}
+            <div className='row'>
+              <h4>Серия паспорта</h4>
+              <h6>Jeck</h6>
+            </div>
+            {/*  */}
+            <div className='row'>
+              <h4>Номер паспорта</h4>
+              <h6>Jeck</h6>
+            </div>
+            {/*  */}
+            <div className='row'>
+              <h4>Срок действия</h4>
+              <h6>Jeck</h6>
+            </div>
+          </div>
+        </div>
+        {/*  */}
+        {/*  */}
+        <div className='card'>
+          <div className='childCard'>
+            <p>Кем выдан*</p>
+            <input
+              className='label'
+              type='text'
+              placeholder='Напишите кем выдан'
+            />
+            <br />
+            <br />
+            <p>Дата выдачи*</p>
+            <input className='label' type='text' placeholder='ДД.ММ.ГГГГ' />
+          </div>
+        </div>
+        {/*  */}
+        {/*  */}
+        <div className='child'>
+          <button className='btn'>Продолжить </button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
